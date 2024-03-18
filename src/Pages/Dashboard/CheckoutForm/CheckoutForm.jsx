1bd0b6777b6a5303d19238416f0eda11cc3ee741
unknown <mahmudul.cse.eng@gmail.com>
2024-03-19 00:41:08 +0600

    post price with intent

--- src/Pages/Dashboard/CheckoutForm/CheckoutForm.jsx
+++ src/Pages/Dashboard/CheckoutForm/CheckoutForm.jsx
@@ -1,12 +1,30 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
+import useCart from "../../../hooks/useCart";
+import { axiosSecure } from "../../../hooks/useAxiosSecure";
 
 
 const CheckoutForm = () => {
     const [errorMessage, setErrorMessage] =useState();
+    const [clientSecret, setClientSecret] = useState();
     const stripe = useStripe();
     const elements = useElements();
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((total, item)=> total + item.price ,0);
+    console.log(totalPrice);
+
+useEffect(()=>{
+
+    axiosSecure.post('/create-payment-intent', {price: totalPrice})
+    .then(res =>{
+        setClientSecret(res.data.clientSecret);
+        console.log(res.data);
+    })
+
+},[totalPrice]);
+
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -59,7 +77,7 @@ const CheckoutForm = () => {
             />
             <div className="text-red-600 mt-6">{errorMessage}</div>
             <div className="text-center">
-            <button className="btn btn-primary mt-8  px-8" type="submit" disabled={!stripe}>
+            <button className="btn btn-primary mt-8  px-8" type="submit" disabled={!stripe && clientSecret }>
                 Pay
             </button>
             </div>
